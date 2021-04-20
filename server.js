72b0698fe9c427512d05f677be35c6283acbf887
eid-sukariah <iameidsukariah@gmail.com>
2021-04-20 13:13:50 +0300

    exam not done!

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 'use strict'
+
+// i have this problem :Error: Forbidden ;and could not solve it so i didn't finish.
+
+
+// Environment variables
+require('dotenv').config();
+
 // Application Dependencies
 const express = require('express');
 const pg = require('pg');
@@ -6,37 +13,53 @@ const methodOverride = require('method-override');
 const superagent = require('superagent');
 const cors = require('cors');
 
-// Environment variables
-require('dotenv').config();
 
 // Application Setup
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
 
 // Express middleware
 // Utilize ExpressJS functionality to parse the body of the request
-
+app.use(express.urlencoded({extended : true}))
 // Specify a directory for static resources
-
+app.use(express.static('./public'))
 // define our method-override reference
-
+// app.use(methodOverride, ('_method'));
 // Set the view engine for server-side templating
-
+app.set('view engine', 'ejs');
 // Use app cors
-
+app.use(cors())
 
 // Database Setup
 const client = new pg.Client(process.env.DATABASE_URL);
 
 // app routes here
 // -- WRITE YOUR ROUTES HERE --
+app.get('/', getAllData);
+// app.get('')
 
 
 // callback functions
 // -- WRITE YOUR CALLBACK FUNCTIONS FOR THE ROUTES HERE --
-
+function getAllData(req, res){
+    
+    const url = `https://thesimpsonsquoteapi.glitch.me/quotes?count=10`;
+    
+    console.log('efwef');
+    superagent.get(url).then(x => {
+        console.log(x.body);
+        // const apiData = x.body.map(obj => new Qutes(obj));
+        // res.send('dgs')
+        // res.render('home', {data: apiData});
+    }).catch(error => console.log(`api error ${error}`))
+}
 // helper functions
-
+function Qutes(info){
+    this.quote = info.quote;
+    this.character = info.character;
+    this.image = info.image;
+    this.characterDirection = info.characterDirection;
+}
 // app start point
 client.connect().then(() =>
     app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
